289812ff86a405f78d405476f1fbc963a0ea1dbb
Kim HyunSik <hsik0225@gmail.com>
2020-05-18 17:41:45 +0900

    [#1] feat: .gitignore 추가

---
+++ BE/src/test/java/com/codesquad/airbnb/AirbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesquad.airbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
