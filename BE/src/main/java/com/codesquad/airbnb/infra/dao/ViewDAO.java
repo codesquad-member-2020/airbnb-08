c64e4770011fda75335e3ac0ac4590bfa056c9dc
Kim HyunSik <hsik0225@gmail.com>
2020-05-21 20:01:04 +0900

    [#16] feat: Main화면 쿼리 작성

--- BE/src/main/java/com/codesquad/airbnb/infra/dao/ViewDAO.java
+++ BE/src/main/java/com/codesquad/airbnb/infra/dao/ViewDAO.java
@@ -1,5 +1,6 @@
 package com.codesquad.airbnb.infra.dao;
 
+import com.codesquad.airbnb.domain.dto.Budget;
 import com.codesquad.airbnb.domain.dto.Main;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -19,10 +20,24 @@ public class ViewDAO {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public List<Main> main() {
+    public List<Main> main(Budget budget) {
+        int lowestPrice = budget == null ? 0 : budget.getLowestPrice();
+        int highestPrice = budget == null ? Integer.MAX_VALUE : budget.getHighestPrice();
 
-        String sql = ""; +
-
+        String sql = "SELECT r.room_id, " +
+                "r.room_name, " +
+                "r.room_picture_url, " +
+                "IF(r.host_is_superhost = 't', '슈퍼호스트', ''), " +
+                "l.country, p.price, " +
+                "p.security_deposit, " +
+                "p.cleaning_fee, " +
+                "r2.number_of_reviews, " +
+                "r2.review_scores_rating " +
+                "FROM rooms r " +
+                "INNER JOIN locations l on r.room_id = l.room_id " +
+                "INNER JOIN prices p on r.room_id = p.room_id " +
+                "INNER JOIN reviews r2 on r.room_id = r2.room_id " +
+                "WHERE p.price BETWEEN ? AND ?;";
 
         RowMapper<Main> mainDTORowMapper = new RowMapper<Main>() {
             @Override
@@ -31,6 +46,6 @@ public class ViewDAO {
             }
         };
 
-        return this.jdbcTemplate.query(sql, mainDTORowMapper);
+        return this.jdbcTemplate.query(sql, new Object[]{lowestPrice, highestPrice}, mainDTORowMapper);
     }
 }
