aae2119ce4250ae3c42630d46a21bc76a3202ef1
Kim HyunSik <hsik0225@gmail.com>
2020-05-21 18:08:09 +0900

    [#16] feat: CORS ERROR 구현

---
+++ BE/src/main/java/com/codesquad/airbnb/ui/MainController.java
@@ -0,0 +1,13 @@
+package com.codesquad.airbnb.ui;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/main")
+public class MainController {
+
+
+}
