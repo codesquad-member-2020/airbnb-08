24a401da6c3371949f1ff9c8894dc0c2183d8b2c
choisohyun <sohyun4283@naver.com>
2020-05-21 17:39:13 +0900

    [#10] feat: 버튼 컴포넌트 스타일 지정
    - 동그랗게, 호버될 때 스타일 적용함

--- FE/src/components/Main/FilterButton/FilterButton.tsx
+++ FE/src/components/Main/FilterButton/FilterButton.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  border-radius: 20px;
+  padding: 10px 15px;
+  margin: 10px 5px;
+  box-sizing: border-box;
+  box-shadow: 0 0 0 1px ${(props) => props.theme.subColor};
+  &:hover {
+    box-shadow: 0 0 0 2px ${(props) => props.theme.mainColor};
+  }
+`;
 
 const FilterButton = () => {
   return (
     <>
-      <Wrapper>TEST</Wrapper>
+      <Wrapper>게스트 4명</Wrapper>
     </>
   );
 };
