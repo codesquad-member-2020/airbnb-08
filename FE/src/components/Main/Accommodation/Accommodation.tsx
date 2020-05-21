d90b68c2bb195263a0b58d7e45b3f5c4f11f0cfa
랄랄랄 <hyewon3938@naver.com>
2020-05-21 18:04:53 +0900

    [#10] feat : Accommodation 컴포넌트 스타일 구조 추가
    - styled component 틀 추가

--- FE/src/components/Main/Accommodation/Accommodation.tsx
+++ FE/src/components/Main/Accommodation/Accommodation.tsx
@@ -1,7 +1,63 @@
 import React from "react";
+import styled from "styled-components";
+import img from "@/test.png";
 
 const Accommodation = () => {
-  return <div></div>;
+  const Wrapper = styled.div``;
+
+  const RoomImage = styled.img``;
+
+  const RoomInfoWrapper = styled.div``;
+
+  const BadgeCountryWrapper = styled.div``;
+
+  const Badge = styled.div``;
+
+  const Country = styled.div``;
+
+  const Rating = styled.div``;
+
+  const Title = styled.div``;
+
+  const PriceWrapper = styled.div``;
+
+  const OriginalPrice = styled.div``;
+
+  const Price = styled.div`
+    ::after {
+      content: "/1박";
+    }
+  `;
+
+  const PriceReservationWrapper = styled.div``;
+
+  const TotalPrice = styled.div``;
+
+  const ReservationButton = styled.button``;
+
+  return (
+    <>
+      <Wrapper>
+        <RoomImage src={img} />
+        <RoomInfoWrapper>
+          <BadgeCountryWrapper>
+            <Badge>슈퍼호스트</Badge>
+            <Country>프랑스</Country>
+          </BadgeCountryWrapper>
+          <Rating>★ 4.89</Rating>
+        </RoomInfoWrapper>
+        <Title>CHARMING HOUSE SEASIDE</Title>
+        <PriceWrapper>
+          <OriginalPrice>￦271,287</OriginalPrice>
+          <Price>￦239,816</Price>
+        </PriceWrapper>
+        <PriceReservationWrapper>
+          <TotalPrice>총 요금 : ￦3,357,426(?)</TotalPrice>
+          <ReservationButton>예약</ReservationButton>
+        </PriceReservationWrapper>
+      </Wrapper>
+    </>
+  );
 };
 
 export default Accommodation;
