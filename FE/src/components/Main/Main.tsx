6143bbce09cbcad335ae40fa6eb03014466e11a8
choisohyun <sohyun4283@naver.com>
2020-05-21 17:38:34 +0900

    [#10] feat: Main에서 버튼 컴포넌트 부르는 코드 추가
    - 메인에서 버튼 컴포넌트 3개 불러오고 기본 스타일 지정함

--- FE/src/components/Main/Main.tsx
+++ FE/src/components/Main/Main.tsx
@@ -1,19 +1,31 @@
 import React from "react";
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
 import reset from "styled-reset";
 import Accommodation from "@Main/Accommodation/Accommodation";
 import FilterButton from "@Main/FilterButton/FilterButton";
+import theme from "@/style/theme";
 
 const Main = () => {
   const StyleReset = createGlobalStyle`
     ${reset};
   `;
 
+  const FilterButtonWrapper = styled.div`
+    display: flex;
+    box-sizing: border-box;
+  `;
+
   return (
     <>
-      <StyleReset />
-      <FilterButton></FilterButton>
-      <Accommodation></Accommodation>
+      <ThemeProvider theme={theme}>
+        <StyleReset />
+        <FilterButtonWrapper>
+          <FilterButton></FilterButton>
+          <FilterButton></FilterButton>
+          <FilterButton></FilterButton>
+        </FilterButtonWrapper>
+        <Accommodation></Accommodation>
+      </ThemeProvider>
     </>
   );
 };
