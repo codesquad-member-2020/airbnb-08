dfcd41c3cfb723a0660fd28fe5e75d60686f1d98
choisohyun <sohyun4283@naver.com>
2020-05-22 13:44:40 +0900

    [#18] feat: common에 작성한 중복 설정 제거
    - common으로 옮긴 중복 설정을 제거함

--- FE/webpack.dev.js
+++ FE/webpack.dev.js
@@ -1,14 +1,12 @@
 const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-module.exports = {
+module.exports = merge(common, {
   mode: "development",
 
   devtool: "cheap-module-source-map",
-
-  entry: "./src/index.tsx",
 
   devServer: {
     historyApiFallback: true,
@@ -18,50 +16,9 @@ module.exports = {
     publicPath: "/",
   },
 
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: ["babel-loader", "ts-loader"],
-      },
-      {
-        test: /\.(jpg|png)$/,
-        loader: "file-loader",
-        options: {
-          name: "[name].[ext]?[hash]",
-        },
-      },
-      {
-        test: /\.jsx?$/,
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env"],
-        },
-        exclude: /node_modules/,
-      },
-    ],
-  },
-
-  resolve: {
-    extensions: [".js", "jsx", ".ts", ".tsx"],
-    alias: {
-      "@": path.resolve(__dirname, "src/"),
-      "@Main": path.resolve(__dirname, "src/components/Main/"),
-      "@CalendarModal": path.resolve(__dirname, "src/components/CalendarModal/"),
-      "@PriceModal": path.resolve(__dirname, "src/components/PriceModal/"),
-      "@GuestCountModal": path.resolve(__dirname, "src/components/GuestCountModal/"),
-      "@ReservationModal": path.resolve(__dirname, "src/components/ReservationModal/"),
-    },
-  },
-
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "public/index.html",
-    }),
     new Dotenv({
       path: path.resolve(__dirname, "./.env.development"),
     }),
   ],
-};
+});
