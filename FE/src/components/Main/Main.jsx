b41ca71159c966c55cea96228a92dcf918d41918
choisohyun <sohyun4283@naver.com>
2020-05-25 16:53:01 +0900

    [#24] feat: 버튼 분기 처리 코드 추가

--- FE/src/components/Main/Main.jsx
+++ FE/src/components/Main/Main.jsx
@@ -1,37 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
 import reset from "styled-reset";
 import Header from "@/components/Header/Header";
 import Accommodation from "@/components/Main/Accommodation/Accommodation";
 import FilterButton from "@/components/Main/FilterButton/FilterButton";
+import GuestCountModal from "@GuestCountModal/GuestCountModal";
 import theme from "@/style/theme";
 
+const StyleReset = createGlobalStyle`
+  ${reset};
+`;
+
+const Wrapper = styled.div`
+  width: 1200px;
+  margin: auto;
+`;
+
+const ResultTitle = styled.div`
+  margin: 40px 0px 10px 10px;
+  font-size: 24px;
+  font-weight: bold;
+  color: ${(props) => props.theme.mainColor};
+`;
+
+const FilterButtonWrapper = styled.div`
+  display: flex;
+  box-sizing: border-box;
+`;
+
+const AccommodationWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
 const Main = () => {
-  const StyleReset = createGlobalStyle`
-    ${reset};
-  `;
+  const [dateVisible, setDateVisible] = useState(false);
+  const [guestVisible, setGuestVisible] = useState(false);
+  const [priceVisible, setPriceVisible] = useState(false);
 
-  const Wrapper = styled.div`
-    width: 1200px;
-    margin: auto;
-  `;
+  const filterButtonClickHandler = (modal) => {
+    console.log("click", modal);
+    switch (modal) {
+      case "date":
+        setDateVisible(!dateVisible);
+        break;
+      case "guest":
+        setGuestVisible(!guestVisible);
+        break;
+      case "price":
+        setPriceVisible(!priceVisible);
+        break;
 
-  const ResultTitle = styled.div`
-    margin: 40px 0px 10px 10px;
-    font-size: 24px;
-    font-weight: bold;
-    color: ${(props) => props.theme.mainColor};
-  `;
-
-  const FilterButtonWrapper = styled.div`
-    display: flex;
-    box-sizing: border-box;
-  `;
-
-  const AccommodationWrapper = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-  `;
+      default:
+        break;
+    }
+  };
 
   return (
     <>
@@ -40,10 +63,23 @@ const Main = () => {
           <StyleReset />
           <Header />
           <FilterButtonWrapper>
-            <FilterButton></FilterButton>
-            <FilterButton></FilterButton>
-            <FilterButton></FilterButton>
+            <FilterButton
+              filterButtonClickHandler={filterButtonClickHandler}
+              dateVisible={dateVisible}
+              modal="date"
+            ></FilterButton>
+            <FilterButton
+              filterButtonClickHandler={filterButtonClickHandler}
+              guestVisible={guestVisible}
+              modal="guest"
+            ></FilterButton>
+            <FilterButton
+              filterButtonClickHandler={filterButtonClickHandler}
+              priceVisible={priceVisible}
+              modal="price"
+            ></FilterButton>
           </FilterButtonWrapper>
+          {/* {dateVisible && <GuestCountModal />} */}
           <ResultTitle>300개 이상의 숙소</ResultTitle>
           <AccommodationWrapper>
             <Accommodation></Accommodation>
