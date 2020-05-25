91d2602f0549282a77cba84eb8d637227a8f7f5f
choisohyun <sohyun4283@naver.com>
2020-05-25 16:50:18 +0900

    [#24] feat: modal이 제대로 나오는지 테스트하기 위해 modal type 넣기
    - modal type을 넣어줌

--- FE/src/components/GuestCountModal/GuestCountModal.jsx
+++ FE/src/components/GuestCountModal/GuestCountModal.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 
-const GuestCountModal = () => {
-  return <></>;
+const Wrapper = styled.div`
+  width: 500px;
+  height: 400px;
+  border: 1px solid red;
+`;
+
+const GuestCountModal = ({ modal }) => {
+  return <Wrapper>{modal}</Wrapper>;
 };
 
 export default GuestCountModal;
