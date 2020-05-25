0a3cf82e98bf03a7ea7a3326bb4bbb5a071b7081
Kim HyunSik <hsik0225@gmail.com>
2020-05-25 12:19:50 +0900

    [#23] feat: Application.class에 SpringApplicationBuilder 추가

--- BE/src/main/java/com/codesquad/airbnb/AirbnbApplication.java
+++ BE/src/main/java/com/codesquad/airbnb/AirbnbApplication.java
@@ -2,12 +2,18 @@ package com.codesquad.airbnb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AirbnbApplication {
+public class AirbnbApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AirbnbApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AirbnbApplication.class);
+    }
 }
