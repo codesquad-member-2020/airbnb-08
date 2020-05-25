740ba94aeb1a8a0090d1b91a772e0f41307a1ffb
choisohyun <sohyun4283@naver.com>
2020-05-25 22:19:15 +0900

    [#24] feat: 최소 0명부터 최대 8명까지 선택되도록 예외처리 추가

--- FE/src/reducers/reducers.js
+++ FE/src/reducers/reducers.js
@@ -8,35 +8,41 @@ const initialState = {
 const guestCountReducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADULT_INCREMENT_COUNT":
+      if (state.adultCount === 8) return state;
       return {
         ...state,
         adultCount: state.adultCount + 1,
         totalCount: state.totalCount + 1,
       };
     case "ADULT_DECREMENT_COUNT":
+      if (state.adultCount === 0) return state;
       return {
         ...state,
         adultCount: state.adultCount - 1,
         totalCount: state.totalCount - 1,
       };
     case "CHILDREN_INCREMENT_COUNT":
+      if (state.childrenCount === 8) return state;
       return {
         ...state,
         childrenCount: state.childrenCount + 1,
         totalCount: state.totalCount + 1,
       };
     case "CHILDREN_DECREMENT_COUNT":
+      if (state.childrenCount === 0) return state;
       return {
         ...state,
         childrenCount: state.childrenCount - 1,
         totalCount: state.totalCount - 1,
       };
     case "BABY_INCREMENT_COUNT":
+      if (state.babyCount === 8) return state;
       return {
         ...state,
         babyCount: state.babyCount + 1,
       };
     case "BABY_DECREMENT_COUNT":
+      if (state.babyCount === 0) return state;
       return {
         ...state,
         babyCount: state.babyCount - 1,
