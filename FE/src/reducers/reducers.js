907604d5aa6e8edee632c03f1d2cf3a44675d59f
choisohyun <sohyun4283@naver.com>
2020-05-25 21:13:49 +0900

    [#24] feat: 타입에 맞게 리듀서 분기처리 추가

--- FE/src/reducers/reducers.js
+++ FE/src/reducers/reducers.js
@@ -1,5 +1,49 @@
-const initialState = {};
+const initialState = {
+  adultCount: 0,
+  childrenCount: 0,
+  babyCount: 0,
+  totalCount: 0,
+};
 
-const guestCountReducer = () => {};
+const guestCountReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADULT_INCREMENT_COUNT":
+      return {
+        ...state,
+        adultCount: state.adultCount + 1,
+        totalCount: state.totalCount + 1,
+      };
+    case "ADULT_DECREMENT_COUNT":
+      return {
+        ...state,
+        adultCount: state.adultCount - 1,
+        totalCount: state.totalCount - 1,
+      };
+    case "CHILDREN_INCREMENT_COUNT":
+      return {
+        ...state,
+        childrenCount: state.childrenCount + 1,
+        totalCount: state.totalCount + 1,
+      };
+    case "CHILDREN_DECREMENT_COUNT":
+      return {
+        ...state,
+        childrenCount: state.childrenCount - 1,
+        totalCount: state.totalCount - 1,
+      };
+    case "BABY_INCREMENT_COUNT":
+      return {
+        ...state,
+        babyCount: state.babyCount + 1,
+      };
+    case "BABY_DECREMENT_COUNT":
+      return {
+        ...state,
+        babyCount: state.babyCount - 1,
+      };
+    default:
+      return state;
+  }
+};
 
 export { guestCountReducer };
