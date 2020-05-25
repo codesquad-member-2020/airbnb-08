26a65449f8062b93c4dbb06c63819c32394fb292
랄랄랄 <hyewon3938@naver.com>
2020-05-25 20:36:35 +0900

    [#24] feat : 인원 모달창 데이터 관리를 위한 redux 설정
    - 인원 모달창에서 redux로 데이터를 관리하기 위해 reducers, actions, 파일 생성
    - store 만들어서 provider에 넣어줌.

--- FE/src/index.jsx
+++ FE/src/index.jsx
@@ -1,10 +1,22 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import App from "@/App";
+
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { guestCountReducer } from "@/reducers/reducers";
+
 const rootElement = document.getElementById("root");
+
+const store = createStore(guestCountReducer);
 
 // import(/*webpackChunkName: 'app' */ "@/App").then(({ default: App }) =>
 //   ReactDOM.render(<App />, rootElement)
 // );
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
