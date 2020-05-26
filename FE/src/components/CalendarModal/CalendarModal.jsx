7ad205215041c9387908def5fafc3bf41192f334
choisohyun <sohyun4283@naver.com>
2020-05-26 17:43:42 +0900

    [#25] feat: 달력 기본 틀 추가

--- FE/src/components/CalendarModal/CalendarModal.jsx
+++ FE/src/components/CalendarModal/CalendarModal.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import moment from "moment";
+import "react-dates/initialize";
+import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from "react-dates";
 
 const CalendarModal = () => {
-  return <div></div>;
+  return (
+    <>
+      <DayPickerRangeController
+        startDate={moment("2020-05-26", "YYYY-MM-DD")} // momentPropTypes.momentObj or null,
+        endDate={moment("2020-06-01", "YYYY-MM-DD")} // momentPropTypes.momentObj or null,
+        onDatesChange={({ startDate, endDate }) => useState({ startDate, endDate })} // PropTypes.func.isRequired,
+        focusedInput={null} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+        onFocusChange={(focusedInput) => useState({ focusedInput })} // PropTypes.func.isRequired,
+        initialVisibleMonth={() => moment().add(2, "M")}
+      />
+    </>
+  );
 };
 
 export default CalendarModal;
