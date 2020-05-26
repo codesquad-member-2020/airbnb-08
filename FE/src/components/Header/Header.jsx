34b7a28fad65e90b88e0273bdfc319dc09462147
choisohyun <sohyun4283@naver.com>
2020-05-26 15:34:09 +0900

    [#24] fix: style 코드 컴포넌트 밖으로 이동
    - devtool에서 경고창이 떠서 밖으로 이동함

--- FE/src/components/Header/Header.jsx
+++ FE/src/components/Header/Header.jsx
@@ -1,49 +1,49 @@
 import React from "react";
 import styled from "styled-components";
 
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+const Logo = styled.img`
+  width: 130px;
+`;
+
+const MenuWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const MenuIcon = styled.div`
+  display: flex;
+`;
+
+const MenuImg = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+const MenuIconBtn = styled.div`
+  font-size: 13px;
+  font-weight: bold;
+  margin-left: 5px;
+`;
+
+const MenuContent = styled.div`
+  padding: 10px 15px;
+  height: 40px;
+  line-height: 20px;
+  border-radius: 15px;
+  margin-left: 3px;
+  font-size: 13px;
+  font-weight: bold;
+  box-sizing: border-box;
+  &:hover {
+    background: #f2f2f2;
+  }
+`;
+
 const Header = () => {
-  const Wrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 20px;
-  `;
-  const Logo = styled.img`
-    width: 130px;
-  `;
-
-  const MenuWrapper = styled.div`
-    display: flex;
-    align-items: center;
-  `;
-
-  const MenuIcon = styled.div`
-    display: flex;
-  `;
-
-  const MenuImg = styled.img`
-    width: 20px;
-    height: 20px;
-  `;
-  const MenuIconBtn = styled.div`
-    font-size: 13px;
-    font-weight: bold;
-    margin-left: 5px;
-  `;
-
-  const MenuContent = styled.div`
-    padding: 10px 15px;
-    height: 40px;
-    line-height: 20px;
-    border-radius: 15px;
-    margin-left: 3px;
-    font-size: 13px;
-    font-weight: bold;
-    box-sizing: border-box;
-    &:hover {
-      background: #f2f2f2;
-    }
-  `;
-
   return (
     <>
       <Wrapper>
