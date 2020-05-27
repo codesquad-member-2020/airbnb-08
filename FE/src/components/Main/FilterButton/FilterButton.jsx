6b80f2778dcfb6167e312c71cbcaef0485f78bae
랄랄랄 <hyewon3938@naver.com>
2020-05-27 16:07:05 +0900

    [#26] feat : 체크인 체크아웃 날짜 클릭 시 필터 버튼에 클릭한 날짜 데이터 표시하는 기능 구현
    - 체크인 체크아웃 날짜 클릭 할 때마다 필터 버튼에 데이터 표시

--- FE/src/components/Main/FilterButton/FilterButton.jsx
+++ FE/src/components/Main/FilterButton/FilterButton.jsx
@@ -5,6 +5,7 @@ import CalendarModal from "@CalendarModal/CalendarModal";
 import PriceModal from "@PriceModal/PriceModal";
 import { useDispatch, useSelector } from "react-redux";
 import * as actions from "@/actions/actions";
+import moment from "moment";
 
 const Wrapper = styled.div`
   border-radius: 20px;
@@ -24,12 +25,17 @@ const FilterButton = ({
   priceVisible,
   modal,
 }) => {
-  const { totalCount, babyCount } = useSelector((state) => state);
+  const { guestCountReducer, datePickerReducer } = useSelector((state) => state);
+  const { totalCount, babyCount } = guestCountReducer;
+  const { startDate, endDate } = datePickerReducer;
 
-  const showGuestCount = () => {
+  const showResult = () => {
     switch (modal) {
       case "date":
-        return "날짜";
+        if (!startDate && !endDate) return "날짜";
+        const start = moment(startDate).format("yyyy-MM-DD");
+        const end = moment(endDate).format("yyyy-MM-DD");
+        return start && !endDate ? `${start} - 체크아웃` : `${start} - ${end}`;
       case "guest":
         if (!totalCount && !babyCount) return "인원";
         return totalCount && !babyCount
@@ -49,9 +55,13 @@ const FilterButton = ({
           filterButtonClickHandler(modal);
         }}
       >
-        {showGuestCount()}
+        {showResult()}
       </Wrapper>
-      {dateVisible && <CalendarModal modal={modal} closeClickHandler={filterButtonClickHandler} />}
+      <CalendarModal
+        dateVisible={dateVisible}
+        modal={modal}
+        closeClickHandler={filterButtonClickHandler}
+      />
       {guestVisible && (
         <GuestCountModal modal={modal} closeClickHandler={filterButtonClickHandler} />
       )}
