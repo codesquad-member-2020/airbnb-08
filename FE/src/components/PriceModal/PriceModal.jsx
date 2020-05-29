b5354bd25953743299461301f207fb19bd59fb65
choisohyun <sohyun4283@naver.com>
2020-05-29 14:17:54 +0900

    [#28] feat: 요금 선택 값 삭제하는 핸들러 로직 추가

--- FE/src/components/PriceModal/PriceModal.jsx
+++ FE/src/components/PriceModal/PriceModal.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import PriceRangeChart from "@PriceModal/PriceRangeChart/PriceRangeChart";
 import SelectedRange from "@PriceModal/SelectedRange/SelectedRange";
+import ModalButtons from "@/components/ModalButtons";
+import { useDispatch } from "react-redux";
+import { deletePriceRange } from "@/actions/priceRangeAction";
 
 const Modal = styled.div`
   position: absolute;
   width: 500px;
-  height: 450px;
+  height: 370px;
   top: 55px;
   left: 5px;
   background: white;
@@ -18,11 +21,21 @@ const Modal = styled.div`
   align-items: center;
 `;
 
-const PriceModal = () => {
+const PriceModal = ({ closeClickHandler }) => {
+  const dispatch = useDispatch();
+
+  const deleteClickHandler = () => {
+    dispatch(deletePriceRange());
+  };
+
   return (
     <Modal>
       <PriceRangeChart></PriceRangeChart>
       <SelectedRange></SelectedRange>
+      <ModalButtons
+        deleteClickHandler={deleteClickHandler}
+        closeClickHandler={closeClickHandler}
+      ></ModalButtons>
     </Modal>
   );
 };
