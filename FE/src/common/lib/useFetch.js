b53eeb5c20fca55f54e1adfbd1a18e34cc0ce459
랄랄랄 <hyewon3938@naver.com>
2020-06-02 14:58:49 +0900

    [#40] feat : useFetch 실행할 때 params와 method를 파라미터로 받아 axios 실행하도록 구현
    - params와 method를 useFetch의 파라미터로 넣어주어 axios 실행할 때 사용

--- FE/src/common/lib/useFetch.js
+++ FE/src/common/lib/useFetch.js
@@ -2,18 +2,20 @@
 
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { useSelector } from "react-redux";
 
-const useFetch = (api) => {
+const useFetch = (api, method, params) => {
   const [loading, setLoading] = useState(false);
   const [resolved, setResolved] = useState(null);
   const [error, setError] = useState(null);
+  const testData = useSelector((state) => state);
 
   useEffect(() => {
     setLoading(true);
-    axios
-      .get(api)
+    axios({ method: method, url: api, params: params })
       .then((response) => {
         setResolved(response.data);
+        console.log(response.data);
         setLoading(false);
       })
       .catch((e) => setError(e));
