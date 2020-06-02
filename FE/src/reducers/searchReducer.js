1b50c93f43c78a765409b54019a87f76b86b9e77
랄랄랄 <hyewon3938@naver.com>
2020-06-02 15:00:48 +0900

    [#40] feat : searchReducer 추가
    - 각 필터 모달에서 저장 버튼 클릭 시에만 데이터 패치를 받아오도록 저장했는지 boolean 값을 가지는 searchReducer추가

---
+++ FE/src/reducers/searchReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+  isSearched: false,
+};
+
+const searchReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "search":
+      return {
+        ...state,
+        isSearched: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export { searchReducer };
