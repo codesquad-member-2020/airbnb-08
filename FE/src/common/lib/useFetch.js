5c24feb6a9340322a07fd22bb8396defd0e2a02d
choisohyun <sohyun4283@naver.com>
2020-06-02 15:45:04 +0900

    [#40] fix: Main에서 작성한 로직을 useFetch로 이동
    - useFetch로 이동시켜 Main에서는 불러와서 쓰게만 하도록 변경함

--- FE/src/common/lib/useFetch.js
+++ FE/src/common/lib/useFetch.js
@@ -2,24 +2,31 @@
 
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { search } from "@/actions/searchAction";
 
 const useFetch = (api, method, params) => {
   const [loading, setLoading] = useState(false);
   const [resolved, setResolved] = useState(null);
   const [error, setError] = useState(null);
-  const testData = useSelector((state) => state);
+  const {
+    searchReducer: { isSearched },
+  } = useSelector((state) => state);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
+    if (isSearched === false) return;
+
     setLoading(true);
     axios({ method: method, url: api, params: params })
       .then((response) => {
         setResolved(response.data);
-        console.log(response.data);
         setLoading(false);
+        dispatch(search(false));
       })
-      .catch((e) => setError(e));
-  }, [api]);
+      .catch((e) => console.error(e));
+  }, [api, isSearched]);
 
   return [loading, resolved, error];
 };
