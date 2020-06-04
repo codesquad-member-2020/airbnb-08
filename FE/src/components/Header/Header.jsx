a0aaa4ac1cfd67ec461a7af50a1847d4ae278f12
choisohyun <sohyun4283@naver.com>
2020-06-04 14:31:11 +0900

    [#41] feat: 임의로 useState 생성하여 로그아웃, 로그인 스타일 추가
    -

--- FE/src/components/Header/Header.jsx
+++ FE/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { API_URL } from "@/common/config";
 import logo from "@/image/airbnbLogo.png";
@@ -46,9 +46,39 @@ const MenuContent = styled.div`
   }
 `;
 
+const UserWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 40px;
+  line-height: 20px;
+  border-radius: 15px;
+  padding: 0 3px;
+  margin-left: 3px;
+  font-size: 13px;
+  font-weight: bold;
+  box-sizing: border-box;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.18);
+`;
+
+const UserId = styled.span`
+  margin: 0 7px;
+`;
+
+const UserImage = styled.img`
+  height: 30px;
+  width: 30px;
+  border-radius: 30px;
+`;
+
 const Header = () => {
+  const [isLogin, setLogin] = useState(false);
+
   const loginClickHandler = () => {
-    window.location.href = API_URL.oauth;
+    setLogin(true);
+  };
+
+  const logoutClickandler = () => {
+    setLogin(false);
   };
 
   return (
@@ -65,8 +95,18 @@ const Header = () => {
           <MenuContent>숙소 호스트 되기</MenuContent>
           <MenuContent>체험 호스팅하기</MenuContent>
           <MenuContent>도움말</MenuContent>
-          <MenuContent onClick={loginClickHandler}>로그인</MenuContent>
-          <MenuContent>회원가입</MenuContent>
+
+          {isLogin ? (
+            <UserWrapper onClick={logoutClickandler}>
+              <UserId>라라ㅏ랄</UserId>
+              <UserImage src="https://a0.muscache.com/im/pictures/5282025/91fcaf72_original.jpg?aki_policy=large" />
+            </UserWrapper>
+          ) : (
+            <>
+              <MenuContent onClick={loginClickHandler}>로그인</MenuContent>
+              <MenuContent>회원가입</MenuContent>
+            </>
+          )}
         </MenuWrapper>
       </Wrapper>
     </>
