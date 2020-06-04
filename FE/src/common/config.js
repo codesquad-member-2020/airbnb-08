7d37a9c162a75ba3c62a8a4c7999d53d72775c20
choisohyun <sohyun4283@naver.com>
2020-06-04 13:39:49 +0900

    [#41] feat: oauth API config 파일에 추가
    - oauth 로그인 링크로 연결되는 API 를 env에 추가하고 config에도 추가함

--- FE/src/common/config.js
+++ FE/src/common/config.js
@@ -2,4 +2,5 @@ export const API_URL = {
   main: process.env.URL + process.env.MAIN,
   budget: process.env.URL + process.env.BUDGET,
   reservations: process.env.URL + process.env.RESERVATIONS,
+  oauth: process.env.URL + process.env.OAUTH,
 };
