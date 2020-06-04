1dc8efce390b88e609f8b46b2924cfd7ccc4e8d1
랄랄랄 <hyewon3938@naver.com>
2020-06-04 13:39:22 +0900

    [#41] feat : webpack config 에 performance 추가
    - 용량이 커서 빌드가 안 되는 문제 해결하기 위해 추가

--- FE/webpack.prod.js
+++ FE/webpack.prod.js
@@ -22,6 +22,11 @@ module.exports = merge(common, {
       },
     },
   },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
 
   plugins: [
     new Dotenv({
