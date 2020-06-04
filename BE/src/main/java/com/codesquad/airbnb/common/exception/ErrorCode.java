8752c784ac1dc25ae8ef6d6aafb38612bbe9c270
Kim HyunSik <hsik0225@gmail.com>
2020-06-04 13:36:04 +0900

    [#38] feat: ErrorCode 구현

--- BE/src/main/java/com/codesquad/airbnb/common/exception/ErrorCode.java
+++ BE/src/main/java/com/codesquad/airbnb/common/exception/ErrorCode.java
@@ -6,9 +6,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    // Common
     INVALID_INPUT_VALUE(400, "Invalid Input Value"),
-    METHOD_NOT_ALLOWED(405, "Invalid Input Value"),
+    ENTITY_NOT_FOUND(400, "Entity Not Found"),
+    INVALID_TYPE_VALUE(400, "Invalid Type Value"),
     HANDLE_ACCESS_DENIED(403, "Access is Denied"),
+    METHOD_NOT_ALLOWED(405, "Invalid Input Value"),
+
+    // User
     UNAUTHORIZED(401, "Please Login");
 
     private final int status;
