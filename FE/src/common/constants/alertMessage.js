ae3250960d9cb0a4cfb4743b0484853139c853d5
Joy <hyewon3938@naver.com>
2020-06-05 07:09:16 +0900

    [#51] feat : 알람 모달창 메시지 추가
    - 인원 선택 메시지와 로그인 메시지 추가

--- FE/src/common/constants/alertMessage.js
+++ FE/src/common/constants/alertMessage.js
@@ -1,2 +1,4 @@
 export const DATE_NOT_NULL = "시작과 끝 날짜를 모두 선택해 주세요!";
 export const DATE_FIRST = "날짜를 먼저 선택해 주세요!";
+export const GUEST_FIRST = "인원을 먼저 선택해 주세요!";
+export const LOGIN_FIRST = "예약은 로그인 후에 가능합니다.";
