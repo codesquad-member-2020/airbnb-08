0ac6e0d4bb5a1b436c32ac1aef5f68fcec6e03f9
choisohyun <sohyun4283@naver.com>
2020-06-05 15:26:17 +0900

    [#41] feat: 쿠키의 key를 인자로 주면 value를 반환하는 함수 작성

--- FE/src/components/Header/Header.jsx
+++ FE/src/components/Header/Header.jsx
@@ -117,6 +117,22 @@ const Header = () => {
   setCookie("userId", "choisohyun");
   setCookie("userImage", "https://avatars2.githubusercontent.com/u/30427711?v=4");
 
+  const getCookieValue = (key) => {
+    let cookieKey = key + "=";
+    let result = "";
+    const cookieArr = document.cookie.split(";");
+
+    for (let i = 0; i < cookieArr.length; i++) {
+      if (cookieArr[i][0] === " ") {
+        cookieArr[i] = cookieArr[i].substring(1);
+      }
+
+      if (cookieArr[i].indexOf(cookieKey) === 0) {
+        result = cookieArr[i].slice(cookieKey.length, cookieArr[i].length);
+        return result;
+      }
+    }
+    return result;
   };
 
   return (
