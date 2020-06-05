92fe323a01264c7087e3b105937a68a04d088e3b
Kim HyunSik <hsik0225@gmail.com>
2020-06-05 14:58:44 +0900

    [#38] refactor: JasyptTest 클래스 삭제

--- BE/src/main/java/com/codesquad/airbnb/reservation/ui/ReservationController.java
+++ BE/src/main/java/com/codesquad/airbnb/reservation/ui/ReservationController.java
@@ -6,7 +6,6 @@ import com.codesquad.airbnb.reservation.domain.ReservationDate;
 import com.codesquad.airbnb.room.domain.Confirmation;
 import com.codesquad.airbnb.manager.ManagerDAO;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
