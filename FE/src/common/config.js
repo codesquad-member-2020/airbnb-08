ac9ae812b9cc9d509bf11a20660e5dca97d4c467
Kim HyunSik <hsik0225@gmail.com>
2020-06-09 19:30:14 +0900

    refactor: 백엔드 폴더만 남기기

--- FE/src/common/config.js
+++
@@ -1,6 +0,0 @@
-export const API_URL = {
-  main: process.env.URL + process.env.MAIN,
-  budget: process.env.URL + process.env.BUDGET,
-  reservations: process.env.URL + process.env.RESERVATIONS,
-  oauth: process.env.URL + process.env.OAUTH,
-};
--- FE/src/common/constants/alertMessage.js
+++
@@ -1,5 +0,0 @@
-export const DATE_NOT_NULL = "시작과 끝 날짜를 모두 선택해 주세요!";
-export const DATE_FIRST = "날짜를 먼저 선택해 주세요!";
-export const GUEST_FIRST = "인원을 먼저 선택해 주세요!";
-export const LOGIN_FIRST = "예약은 로그인 후에 가능합니다.";
-export const ADULT_REQUIRE = "게스트가 어린이나 유아만 있는 경우는 예약할 수 없습니다.";
